f28691e963d40a88f07b27889a07a655895651eb
William Blackerby <wm@williamblackerby.com>
2026-03-03 15:24:00 -0500

    improve error handling

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ st.title(TITLE)
 query = st.text_input("Query", "+hello +world")
 try:
     simplified = aqpy.simplify(query)
-    st.write("Simplified query:", simplified)
-except:
-    st.write("Invalid query")
+    st.write("Simplified query:")
+    st.code(f"{simplified}", language=None)
+except RuntimeError as e:
+    st.write("Invalid query:")
+    st.code(f"{e}", language=None)
 
